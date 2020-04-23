2ebcb75aab734d87dcdc48bc7fa6c8ebe1e0084a
rguptavh <rguptavh119@gmail.com>
2020-04-22 22:57:13 -0500

    redid ui and did list filter

--- app/src/main/java/com/example/stt/MainActivity.java
+++ app/src/main/java/com/example/stt/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.*;
 import android.os.Bundle;
 import android.speech.*;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.*;
 import android.view.*;
 import android.widget.*;
@@ -13,16 +15,36 @@ import android.widget.*;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView speechtext;
-    private Button talk;
+    private EditText speechtext;
+    private ImageButton talk;
     private String resulto = "";
+    private ListView lView;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         speechtext = findViewById(R.id.speechtext);
-        talk = (Button)findViewById(R.id.talkbut);
+        talk = findViewById(R.id.talkbut);
+        lView = findViewById(R.id.list);
         //resulto = "";
+        final String[] colorsArray = new String[]{
+                "red", "blue", "green", "black", "white",
+                "gray", "cyan", "magenta", "yellow", "lightgray",
+                "darkgray", "grey", "lightgrey", "darkgrey", "aqua",
+                "fuchsia", "lime", "maroon", "navy", "olive",
+                "purple", "silver", "teal"
+        };
+
+        //Get widgets reference from XML layout
+
+
+        //Initialize an ArrayAdapter and data bind with a String Array
+        adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,colorsArray);
+
+        //Data bind ListView with ArrayAdapter
+        lView.setAdapter(adapter);
         talk.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -34,7 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+        speechtext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (MainActivity.this).adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
